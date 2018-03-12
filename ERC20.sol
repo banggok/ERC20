745e97b3de54a9f1e8c413c68b04e60891243e6c
banggok <rtriasmono@gmail.com>
2018-03-12 21:33:07 +0700

    update contract compatibility with decimal transaction

--- ERC20.sol
+++ ERC20.sol
@@ -17,13 +17,25 @@ contract owned {
     }
 }
 
+contract convert{
+    uint256 decimalCoin;
+    function convert(uint256 dec) public{
+        decimalCoin = dec;
+    }
+    
+    function convertDecimal(uint256 value, uint256 decimal) view internal returns(uint256){
+        return(value * (10 ** (decimalCoin - decimal)));
+    }
+}
+
 contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public; }
 
-contract TokenERC20 {
+contract TokenERC20 is convert {
+    
     // Public variables of the token
     string public name;
     string public symbol;
-    uint8 public decimals = 18;
+    uint8 public decimals;
     // 18 decimals is the strongly suggested default, avoid changing it
     uint256 public totalSupply;
 
@@ -45,21 +57,16 @@ contract TokenERC20 {
     function TokenERC20(
         uint256 initialSupply,
         string tokenName,
-        string tokenSymbol
-    ) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        string tokenSymbol,
+        uint8 _decimals
+    ) convert(_decimals) public {
+        totalSupply = convertDecimal(initialSupply, decimals);  // Update total supply with the decimal amount
         balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
         name = tokenName;                                   // Set the name for display purposes
         symbol = tokenSymbol;                               // Set the symbol for display purposes
+        decimals = _decimals;
     }
-
-    //internal function to convert value to smallest value
-
-    function valueToSmallest(uint256 _value) internal view returns(uint256){
-        _value = _value * 10 ** uint256(decimals);
-        return (_value);
-    }
-
+    
     /**
      * Internal transfer, only can be called by this contract
      */
@@ -72,8 +79,6 @@ contract TokenERC20 {
         require(balanceOf[_to] + _value > balanceOf[_to]);
         // Save this for an assertion in the future
         uint previousBalances = balanceOf[_from] + balanceOf[_to];
-        //convert value to smallest value
-        valueToSmallest(_value);
         // Subtract from the sender
         balanceOf[_from] -= _value;
         // Add the same to the recipient
@@ -92,6 +97,21 @@ contract TokenERC20 {
      * @param _value the amount to send
      */
     function transfer(address _to, uint256 _value) public {
+        _value = convertDecimal(_value, decimals);
+        _transfer(msg.sender, _to, _value);
+    }
+    
+    /**
+     * Transfer tokens
+     *
+     * Send `_value` tokens to `_to` from your account
+     *
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     * @param dec number of decimal. 0.1 means 1 dec 1
+     */
+    function transferDecimal(address _to, uint256 _value, uint256 dec) public {
+        _value = convertDecimal(_value, dec);
         _transfer(msg.sender, _to, _value);
     }
 
@@ -105,6 +125,25 @@ contract TokenERC20 {
      * @param _value the amount to send
      */
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        _value = convertDecimal(_value, decimals);
+        require(_value <= allowance[_from][msg.sender]);     // Check allowance
+        allowance[_from][msg.sender] -= _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+    
+    /**
+     * Transfer tokens from other address
+     *
+     * Send `_value` tokens to `_to` in behalf of `_from`
+     *
+     * @param _from The address of the sender
+     * @param _to The address of the recipient
+     * @param _value the amount to send
+     * @param dec number of decimal. 0.1 means 1 dec 1
+     */
+    function transferFromDecimal(address _from, address _to, uint256 _value, uint256 dec) public returns (bool success) {
+        _value = convertDecimal(_value, dec);
         require(_value <= allowance[_from][msg.sender]);     // Check allowance
         allowance[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
@@ -121,6 +160,23 @@ contract TokenERC20 {
      */
     function approve(address _spender, uint256 _value) public
         returns (bool success) {
+        _value = convertDecimal(_value, decimals);
+        allowance[msg.sender][_spender] = _value;
+        return true;
+    }
+    
+    /**
+     * Set allowance for other address
+     *
+     * Allows `_spender` to spend no more than `_value` tokens in your behalf
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     * @param dec number of decimal. 0.1 means 1 dec 1
+     */
+    function approveDecimal(address _spender, uint256 _value, uint256 dec) public
+        returns (bool success) {
+        _value = convertDecimal(_value, dec);
         allowance[msg.sender][_spender] = _value;
         return true;
     }
@@ -137,6 +193,28 @@ contract TokenERC20 {
     function approveAndCall(address _spender, uint256 _value, bytes _extraData)
         public
         returns (bool success) {
+        _value = convertDecimal(_value, decimals);
+        tokenRecipient spender = tokenRecipient(_spender);
+        if (approve(_spender, _value)) {
+            spender.receiveApproval(msg.sender, _value, this, _extraData);
+            return true;
+        }
+    }
+    
+    /**
+     * Set allowance for other address and notify
+     *
+     * Allows `_spender` to spend no more than `_value` tokens in your behalf, and then ping the contract about it
+     *
+     * @param _spender The address authorized to spend
+     * @param _value the max amount they can spend
+     * @param _extraData some extra information to send to the approved contract
+     * @param dec number of decimal. 0.1 means 1 dec 1
+     */
+    function approveAndCallDecimal(address _spender, uint256 _value, bytes _extraData, uint256 dec)
+        public
+        returns (bool success) {
+        _value = convertDecimal(_value, dec);
         tokenRecipient spender = tokenRecipient(_spender);
         if (approve(_spender, _value)) {
             spender.receiveApproval(msg.sender, _value, this, _extraData);
@@ -152,6 +230,24 @@ contract TokenERC20 {
      * @param _value the amount of money to burn
      */
     function burn(uint256 _value) public returns (bool success) {
+        _value = convertDecimal(_value, decimals);
+        require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
+        balanceOf[msg.sender] -= _value;            // Subtract from the sender
+        totalSupply -= _value;                      // Updates totalSupply
+        emit Burn(msg.sender, _value);
+        return true;
+    }
+    
+    /**
+     * Destroy tokens
+     *
+     * Remove `_value` tokens from the system irreversibly
+     *
+     * @param _value the amount of money to burn
+     * @param dec number of decimal. 0.1 means 1 dec 1
+     */
+    function burnDecimal(uint256 _value, uint256 dec) public returns (bool success) {
+        _value = convertDecimal(_value, dec);
         require(balanceOf[msg.sender] >= _value);   // Check if the sender has enough
         balanceOf[msg.sender] -= _value;            // Subtract from the sender
         totalSupply -= _value;                      // Updates totalSupply
@@ -168,6 +264,27 @@ contract TokenERC20 {
      * @param _value the amount of money to burn
      */
     function burnFrom(address _from, uint256 _value) public returns (bool success) {
+        _value = convertDecimal(_value, decimals);
+        require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
+        require(_value <= allowance[_from][msg.sender]);    // Check allowance
+        balanceOf[_from] -= _value;                         // Subtract from the targeted balance
+        allowance[_from][msg.sender] -= _value;             // Subtract from the sender's allowance
+        totalSupply -= _value;                              // Update totalSupply
+        emit Burn(_from, _value);
+        return true;
+    }
+    
+    /**
+     * Destroy tokens from other account
+     *
+     * Remove `_value` tokens from the system irreversibly on behalf of `_from`.
+     *
+     * @param _from the address of the sender
+     * @param _value the amount of money to burn
+     * @param dec number of decimal. 0.1 means 1 dec 1
+     */
+    function burnFromDecimal(address _from, uint256 _value, uint256 dec) public returns (bool success) {
+        _value = convertDecimal(_value, dec);
         require(balanceOf[_from] >= _value);                // Check if the targeted balance is enough
         require(_value <= allowance[_from][msg.sender]);    // Check allowance
         balanceOf[_from] -= _value;                         // Subtract from the targeted balance
@@ -186,6 +303,7 @@ contract MyAdvancedToken is owned, TokenERC20 {
 
     uint256 public sellPrice;
     uint256 public buyPrice;
+    uint256 internal priceDecimal;
 
     mapping (address => bool) public frozenAccount;
 
@@ -196,8 +314,9 @@ contract MyAdvancedToken is owned, TokenERC20 {
     function MyAdvancedToken(
         uint256 initialSupply,
         string tokenName,
-        string tokenSymbol
-    ) TokenERC20(initialSupply, tokenName, tokenSymbol) public {}
+        string tokenSymbol,
+        uint8 _decimals
+    ) TokenERC20(initialSupply, tokenName, tokenSymbol, _decimals) public {}
 
     /* Internal transfer, only can be called by this contract */
     function _transfer(address _from, address _to, uint _value) internal {
@@ -215,6 +334,19 @@ contract MyAdvancedToken is owned, TokenERC20 {
     /// @param target Address to receive the tokens
     /// @param mintedAmount the amount of tokens it will receive
     function mintToken(address target, uint256 mintedAmount) onlyOwner public {
+        mintedAmount = convertDecimal(mintedAmount, decimals);
+        balanceOf[target] += mintedAmount;
+        totalSupply += mintedAmount;
+        emit Transfer(0, this, mintedAmount);
+        emit Transfer(this, target, mintedAmount);
+    }
+    
+    /// @notice Create `mintedAmount` tokens and send it to `target`
+    /// @param target Address to receive the tokens
+    /// @param mintedAmount the amount of tokens it will receive
+    /// @param dec number of decimal. 0.1 means 1 dec 1
+    function mintTokenDecimal(address target, uint256 mintedAmount, uint256 dec) onlyOwner public {
+        mintedAmount = convertDecimal(mintedAmount, dec);
         balanceOf[target] += mintedAmount;
         totalSupply += mintedAmount;
         emit Transfer(0, this, mintedAmount);
@@ -232,30 +364,48 @@ contract MyAdvancedToken is owned, TokenERC20 {
     /// @notice Allow users to buy tokens for `newBuyPrice` eth and sell tokens for `newSellPrice` eth
     /// @param newSellPrice Price the users can sell to the contract
     /// @param newBuyPrice Price users can buy from the contract
-    function setPrices(uint256 newSellPrice, uint256 newBuyPrice) onlyOwner public {
+    /// @param dec number of decimal. 0.1 means 1 dec 1
+    
+    function setPrices(uint256 newSellPrice, uint256 newBuyPrice, uint8 dec) onlyOwner public {
         sellPrice = newSellPrice;
         buyPrice = newBuyPrice;
+        priceDecimal = dec;
     }
+    
 
     /// @notice Buy tokens from contract by sending ether
     function buy() payable public {
-        uint amount = msg.value / buyPrice;               // calculates the amount
-        _transfer(this, msg.sender, amount);              // makes the transfers
+        uint amount = convertDecimal((msg.value / buyPrice), (decimals - priceDecimal)) ;               // calculates the amount
+        _transfer(owner, msg.sender, amount);              // makes the transfers
     }
 
     /// @notice Sell `amount` tokens to contract
     /// @param amount amount of tokens to be sold
-    function sell(uint256 amount) public {
-        require(address(this).balance >= amount * sellPrice);      // checks if the contract has enough ether to buy
-        _transfer(msg.sender, this, amount);              // makes the transfers
+    /// @param dec number of decimal. 0.1 means 1 dec 1
+    function sell(uint256 amount, uint256 dec) public {
+        amount = convertDecimal(amount, dec);
+        require(owner.balance >= amount * sellPrice);      // checks if the contract has enough ether to buy
+        _transfer(msg.sender, owner, amount);              // makes the transfers
         msg.sender.transfer(amount * sellPrice);          // sends ether to the seller. It's important to do this last to avoid recursion attacks
     }
+    
+    function withdrawn(uint amount) onlyOwner public{
+        uint256 oneEther = 1 ether;
+        amount = amount * oneEther;
+        require(address(this).balance >= amount);
+        owner.transfer(amount);
+    }
+    
+    function contractBalance() public view returns(uint256){
+        return address(this).balance;
+    }
+
 }
 
 //Implement Coin here, change parameter as you need
 
 contract NewCoin is MyAdvancedToken{
-    function NewCoin() MyAdvancedToken(1000000, "BGC20", "B20") public{
+    function NewCoin() MyAdvancedToken(1000000, "BGC20", "B20", 18) public{
         
     }
 }
--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Changes:
 - use Compiler 0.4.21
 - change tokenRecipient type from interface to contract to dismiss warning
 - add 'emit' to Event
-- change this type to address. this.balance -> address(this).balance
-- add implementation parameter to make easier to deploy
-- add valueToSmallest function, to convert transfered value to smallest one
+- extend implementation parameter to make easier to deploy
+- add parameter decimal to setPrice, so it can be set to lower unit
+- add contract convertDecimal to convert any transaction in Decimal
+- duplicate transactional function with capability of decimal
+- add withdrawn function to move contract balance to owner
+- add contractBalance function to check contract balance
